c8e91c5ab6f2fc8bd868eae92fdb99263c9879b6
Anastasia <galanpav33@yandex.ru>
2024-09-04 20:50:54 +0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Item from './Item';
 import './App.css'
+import { Todo } from './types';
 
 const App = () => {
-  const initialTodos = ['sleep', 'learn', 'sleep'];
-  const [todos, setTodos] = useState(initialTodos);
-  const inputRef = useRef<any>(null);
+  const initialTodos: Todo[] = [
+    { id: '1', title: 'sleep' },
+    { id: '2', title: 'learn' },
+    { id: '3', title: 'sleep' },
+  ];
+  const [todos, setTodos] = useState<Todo[]>(JSON.parse(localStorage.getItem('todos') || JSON.stringify(initialTodos)));
+  const inputRef = useRef<HTMLInputElement>(null);
   
 
   const addTodoHandler = (e: React.FormEvent) => {
     e.preventDefault();
-    if (inputRef.current.value) {
-      setTodos([...todos, inputRef.current.value]);
+    if (inputRef.current) {
+      const newTodo: Todo = {
+        id: Date.now(),
+        title: inputRef.current.value,
+      };
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
       inputRef.current.value = '';
     }
   };
+
+  const deleteTodoHandler = (id: number) => {
+    setTodos((prevTodos) => prevTodos.filter(todo => todo.id !== id));
+  };
+
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
@@ -27,10 +41,9 @@ const App = () => {
         <button type="submit" onClick={addTodoHandler}>Добавить</button>
         </form>
     
-        {todos.map((el, index) => (
-        <Item key={index} el={el} />
+        {todos.map((el) => (
+        <Item key={el.id} el={el} onDelete={deleteTodoHandler} />
       ))}
-      {todos.length >= 4 && <div>Всего задач: {todos.length}</div>}
     </div>
     </>
   );
